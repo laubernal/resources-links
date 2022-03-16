33133fc581b534e8b09229505b9590e30611db7f
laubernal <lbernalrodrguez@gmail.com>
2022-03-16 16:51:05 +0100

    created express router and user entity

---
+++ src/Domain/entities/Auth/user.ts
@@ -0,0 +1,35 @@
+import { Id } from '../../vo/Id';
+
+export class User {
+  public static build(firstName: string, lastName: string, email: string, password: string): User {
+    return new User(Id.generate(), firstName, lastName, email, password);
+  }
+
+  constructor(
+    private _id: Id,
+    private _firstName: string,
+    private _lastName: string,
+    private _email: string,
+    private _password: string
+  ) {}
+
+  public get id(): Id {
+    return this._id;
+  }
+
+  public get firstName(): string {
+    return this._firstName;
+  }
+
+  public get lastName(): string {
+    return this._lastName;
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+
+  public get password(): string {
+    return this._password;
+  }
+}
