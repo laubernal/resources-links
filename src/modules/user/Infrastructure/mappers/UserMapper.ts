e3a07fee47002bc6d510a5316009cf872e060994
laubernal <lbernalrodrguez@gmail.com>
2022-03-19 15:55:00 +0100

    created model and mapper for user, a connection with the database and a docker compose

---
+++ src/modules/user/Infrastructure/mappers/UserMapper.ts
@@ -0,0 +1,29 @@
+import { IMapper } from '../../../shared/Infrastructure/mappers/IMapper';
+import { User } from '../../Domain/entities/user.entity';
+import { UserModel } from '../dataModel/UserModel';
+
+export class UserMapper implements IMapper<UserModel, User> {
+  public rawDataToModel(user: any): UserModel[] {
+    const userToModel = [];
+
+    userToModel.push(
+      new UserModel(
+        user.us_id,
+        user.us_first_name,
+        user.us_last_name,
+        user.us_email,
+        user.us_password
+      )
+    );
+
+    return userToModel;
+  }
+
+  public toDomain(user: UserModel): User {
+    return new User(user.id, user.firstName, user.lastName, user.email, user.password);
+  }
+
+  public toData(user: User): UserModel {
+    return new UserModel(user.id, user.firstName, user.lastName, user.email, user.password);
+  }
+}
---
+++ src/modules/user/Infrastructure/repositories/UserRepository.ts
@@ -0,0 +1,14 @@
+import { BaseRepository } from '../../../shared/Infrastructure/repositories/BaseRepository';
+import { User } from '../../Domain/entities/user.entity';
+import { UserModel } from '../dataModel/UserModel';
+import { UserMapper } from '../mappers/UserMapper';
+
+export class UserRepository extends BaseRepository<UserModel, User> {
+  protected mapper = new UserMapper();
+
+  public async save(user: User): Promise<void> {}
+
+  public async getId(email: string): Promise<string> {
+    return '';
+  }
+}
