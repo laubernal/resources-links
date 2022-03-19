6943d21a1b7bb600e992ce56d6ec6124626d6ea5
laubernal <lbernalrodrguez@gmail.com>
2022-03-19 16:24:29 +0100

    created sign in controller

---
+++ src/constants/databaseTablenames.ts
@@ -0,0 +1,1 @@
+export const USERS_TABLENAME = 'users';
--- src/modules/user/Infrastructure/repositories/UserRepository.ts
+++ src/modules/user/Infrastructure/repositories/UserRepository.ts
@@ -1,3 +1,4 @@
+import { USERS_TABLENAME } from '../../../../constants/databaseTablenames';
 import { BaseRepository } from '../../../shared/Infrastructure/repositories/BaseRepository';
 import { User } from '../../Domain/entities/user.entity';
 import { UserModel } from '../dataModel/UserModel';
@@ -5,6 +6,10 @@ import { UserMapper } from '../mappers/UserMapper';
 
 export class UserRepository extends BaseRepository<UserModel, User> {
   protected mapper = new UserMapper();
+
+  constructor() {
+    super(USERS_TABLENAME)
+  }
 
   public async save(user: User): Promise<void> {
     // USE PRISMA
