98530d85620b2c9735153d61f8d2dc98677089a3
laubernal <lbernalrodrguez@gmail.com>
2022-03-23 17:02:08 +0100

    create connection to database with prisma and add prisma implementation to user repository

--- src/modules/shared/Infrastructure/repositories/Database.ts
+++ src/modules/shared/Infrastructure/repositories/Database.ts
@@ -1,29 +1,55 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
+import { PrismaClient } from '@prisma/client';
+import chalk from 'chalk';
 
 export class Database {
-  private static client: Pool;
+  // private static client: Pool;
+  private static _instance: Database;
+  private readonly prismaClient: PrismaClient;
 
-  constructor() {}
-
-  public static instance(): Pool {
-    this.client = new Pool({
-      max: 20,
-      idleTimeoutMillis: 30000,
-      connectionTimeoutMillis: 2000,
-    });
-
-    return this.client;
-  }
-
-  public static async query(query: string, values: any[]) {
-    const pool = Database.instance();
+  constructor() {
+    console.log(chalk.magenta(`Connecting to database...`));
 
     try {
-      await pool.connect();
-
-      return await pool.query(query, values);
+      this.prismaClient = new PrismaClient();
     } catch (error: any) {
-      throw new Error(`Error in Database - ${error.stack}`);
+      throw new Error(chalk.red(`❌ Error connecting to ${process.env.PGDATABASE} database`));
     }
+
+    console.log(chalk.green(`✔ Connected successfully to ${process.env.PGDATABASE} database`));
   }
+
+  public static instance(): Database {
+    if (!Database._instance) {
+      Database._instance = new Database();
+    }
+
+    return Database._instance;
+  }
+
+  public connection(): PrismaClient {
+    return this.prismaClient;
+  }
+
+  // public static instance(): Pool {
+  //   this.client = new Pool({
+  //     max: 20,
+  //     idleTimeoutMillis: 30000,
+  //     connectionTimeoutMillis: 2000,
+  //   });
+
+  //   return this.client;
+  // }
+
+  // public static async query(query: string, values: any[]) {
+  //   const pool = Database.instance();
+
+  //   try {
+  //     await pool.connect();
+
+  //     return await pool.query(query, values);
+  //   } catch (error: any) {
+  //     throw new Error(chalk.red(`Error in Database - ${error.stack}`));
+  //   }
+  // }
 }
--- src/modules/user/Infrastructure/repositories/UserRepository.ts
+++ src/modules/user/Infrastructure/repositories/UserRepository.ts
@@ -1,22 +1,53 @@
-import { USERS_TABLENAME } from '../../../../constants/databaseTablenames';
-import { BaseRepository } from '../../../shared/Infrastructure/repositories/BaseRepository';
+import chalk from 'chalk';
+import { Database } from '../../../shared/Infrastructure/repositories/Database';
 import { User } from '../../Domain/entities/user.entity';
-import { UserModel } from '../dataModel/UserModel';
+import { IUserRepository } from '../../Domain/interfaces/IUserRepository';
 import { UserMapper } from '../mappers/UserMapper';
 
-export class UserRepository extends BaseRepository<UserModel, User> {
+export class UserRepository implements IUserRepository {
   protected mapper = new UserMapper();
+  private prisma = Database.instance().connection();
 
-  constructor() {
-    super(USERS_TABLENAME)
+  constructor() {}
+
+  public async getOneBy(email: string): Promise<User | undefined> {
+    const result = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!result) {
+      return undefined;
+    }
+
+    this.prisma.$disconnect();
+
+    const userToModel = this.mapper.rawDataToModel(result);
+
+    return this.mapper.toDomain(userToModel[0]);
+  }
+
+  public async getAllBy(column: string, value: string): Promise<User | undefined> {
+    return undefined;
   }
 
   public async save(user: User): Promise<void> {
     // USE PRISMA
+    const result = await this.prisma.user.create({
+      data: {
+        id: user.id,
+        first_name: user.firstName,
+        last_name: user.lastName,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    chalk.cyan(console.log(`result ${result}`));
+
+    this.prisma.$disconnect();
   }
 
   public async getId(email: string): Promise<string> {
     // USE PRISMA
+
     return '';
   }
 }
