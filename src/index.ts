250a3a86d939ea2a1679185130438577fa5517ad
laubernal <lbernalrodrguez@gmail.com>
2022-03-28 11:44:02 +0200

    add error handler middleware

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cookieSession from 'cookie-session';
 import express, { Application } from 'express';
 
 import { AppRouter } from './modules/shared/Infrastructure/controllers/AppRouter';
-
+import { errorHandler } from './modules/shared/Infrastructure/middlewares/error-handler';
 import './modules/user/Infrastructure/controllers';
 
 const app: Application = express();
@@ -15,11 +15,12 @@ app.use(
   cookieSession({ name: 'resources-links-session', maxAge: 24 * 60 * 60 * 1000, signed: false })
 );
 app.use(AppRouter.getInstance());
+app.use(errorHandler);
 
 try {
   app.listen(port, () => {
-    console.log(`Connected successfully to port: ${port}`);
+    console.log(`✔ Connected successfully to port: ${port}`);
   });
 } catch (error: any) {
-  console.error(`Error occurred: ${error.message}`);
+  console.error(`❌ Error occurred: ${error.message}`);
 }
