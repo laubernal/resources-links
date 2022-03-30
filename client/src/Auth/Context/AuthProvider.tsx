6439187b0320f52a568a920d0818781c8ca90fa0
laubernal <lbernalrodrguez@gmail.com>
2022-03-30 13:04:05 +0200

    improved auth provider and added box for the link to sign up in sign in component

--- client/src/Auth/Context/AuthProvider.tsx
+++ client/src/Auth/Context/AuthProvider.tsx
@@ -1,57 +1,10 @@
-import axios from 'axios';
 import React from 'react';
 
-import useToken from '../../Shared/Hooks/useToken';
+import { useAuthProvider } from '../Hooks/useAuthProvider';
 import { AuthContext } from './AuthContext';
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const { token, setToken } = useToken();
+  const auth = useAuthProvider();
 
-  const signin = async (email: string, password: string, callback: VoidFunction): Promise<void> => {
-    const response = await axios.post('/signin', { email, password });
-
-    setToken(response.data.userJwt);
-
-    callback();
-  };
-
-  const signup = async (
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-    passwordConfirmation: string,
-    callback: VoidFunction
-  ): Promise<void> => {
-    try {
-      const response = await axios.post('signup', {
-        firstName,
-        lastName,
-        email,
-        password,
-        passwordConfirmation,
-      });
-      setToken(response.data.userJwt);
-
-      callback();
-    } catch (error: any) {
-      console.log(error.message);
-    }
-  };
-
-  const signout = async (callback: VoidFunction) => {
-    try {
-      await axios.get('/signout');
-      setToken(null);
-      callback();
-    } catch (error: any) {
-      console.log(error.message);
-    }
-  };
-
-  return (
-    <AuthContext.Provider value={{ token, signin, signup, signout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
---
+++ client/src/Auth/Hooks/useAuthProvider.tsx
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+import useToken from '../../Shared/Hooks/useToken';
+
+export const useAuthProvider = () => {
+  const { token, setToken } = useToken();
+
+  const signin = async (email: string, password: string, callback: VoidFunction): Promise<void> => {
+    const response = await axios.post('/signin', { email, password });
+
+    setToken(response.data.userJwt);
+
+    callback();
+  };
+
+  const signup = async (
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    passwordConfirmation: string,
+    callback: VoidFunction
+  ): Promise<void> => {
+    try {
+      const response = await axios.post('signup', {
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordConfirmation,
+      });
+      setToken(response.data.userJwt);
+
+      callback();
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  const signout = async (callback: VoidFunction) => {
+    try {
+      await axios.get('/signout');
+      setToken(null);
+      callback();
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  return {
+    signin,
+    signup,
+    signout,
+    token,
+  };
+};
