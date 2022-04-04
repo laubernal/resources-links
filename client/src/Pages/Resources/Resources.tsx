7d9c60d56af1d05156ed5392844fb29ded0285e0
laubernal <lbernalrodrguez@gmail.com>
2022-04-04 12:01:15 +0200

    created header

--- client/src/Pages/Resources/Resources.tsx
+++ client/src/Pages/Resources/Resources.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Grid, Space } from '@mantine/core';
 
 import SignOut from '../../Auth/Components/Auth/SignOut/SignOut';
 import ResourcesLinks from '../../ResourcesLinks/Components/ResourcesLinks';
+import Header from '../../Auth/Components/Header/Header';
 
 function Resources(): JSX.Element {
   return (
     <>
-      <SignOut />
+      <Grid grow={true}>
+        <Grid.Col span={3}>
+          <Header />
+        </Grid.Col>
+        <Grid.Col span={3} offset={6}>
+          <SignOut />
+        </Grid.Col>
+      </Grid>
+      <Space h="md" />
       <ResourcesLinks />
     </>
   );
--- client/src/ResourcesLinks/Components/ResourcesLinks.tsx
+++ client/src/ResourcesLinks/Components/ResourcesLinks.tsx
@@ -2,7 +2,7 @@ import { Title } from '@mantine/core';
 import React from 'react';
 
 function ResourcesLinks(): JSX.Element {
-  return <Title order={1}>Create your first resource</Title>;
+  return <Title order={3}>Create your first resource</Title>;
 }
 
 export default ResourcesLinks;
