4f925e436774db6d15decd74016c92ead6816b60
laubernal <lbernalrodrguez@gmail.com>
2022-04-04 12:01:15 +0200

    created header

---
+++ client/src/Auth/Components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from '@mantine/core';
+
+function Header(): JSX.Element {
+  return <Title order={1}>Resources Links</Title>;
+}
+
+export default Header;
