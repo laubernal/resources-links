bf16b241f203d8fbb9ea972524aba2d61b9b6d5b
laubernal <lbernalrodrguez@gmail.com>
2022-04-05 16:31:18 +0200

    created resources repository

--- resources-links/src/modules/resources/Domain/interfaces/IResourcesRepository.ts
+++ resources-links/src/modules/resources/Domain/interfaces/IResourcesRepository.ts
@@ -2,5 +2,5 @@ import { Resource } from '../entities/resource.entity';
 
 export interface IResourcesRepository {
   save(resource: Resource): Promise<void>;
-  getAllBy(value: string): Promise<Resource | undefined>;
+  getAllBy(value: string): Promise<Resource[] | undefined>;
 }
