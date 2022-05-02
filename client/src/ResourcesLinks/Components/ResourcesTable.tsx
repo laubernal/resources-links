ac00d31e632996a807f0d39c37a1b29ad22822a4
laubernal <lbernalrodrguez@gmail.com>
2022-05-02 18:32:31 +0200

    added text components

--- client/src/ResourcesLinks/Components/ResourcesTable.tsx
+++ client/src/ResourcesLinks/Components/ResourcesTable.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Table, Text, Title } from '@mantine/core';
+import { Link } from 'tabler-icons-react';
 
 import { useResource } from '../Hooks/useResource';
 
@@ -20,33 +21,64 @@ function ResourcesTable(): JSX.Element {
 
   const rows = resource.resourcesList.map(resource => (
     <tr key={resource.id}>
-      <td>{resource.title}</td>
+      <td>
+        <Text align="center">{resource.title}</Text>
+      </td>
       <td>
         <Text variant="link" component="a" href={resource.link} target="_blank">
           {resource.link}
         </Text>
       </td>
-      <td>{resource.note}</td>
       <td>
-        {resource.categories.map(category => {
-          return <p>{category.name}</p>;
-        })}
+        <Text align="center">{resource.note}</Text>
       </td>
-      <td>{resource.createdAt}</td>
+      <td>
+        <Text align="center">
+          {resource.categories.map(category => {
+            return <p>{category.name}</p>;
+          })}
+        </Text>
+      </td>
+      <td>
+        <Text>{resource.createdAt}</Text>
+      </td>
     </tr>
   ));
 
   return (
     <div>
-      <Title order={3}>Create your first resource</Title>
+      <Title order={3}>
+        <Link size={23} strokeWidth={2} color={'black'} />
+        Your resources
+      </Title>
       <Table>
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Link</th>
-            <th>Note</th>
-            <th>Category</th>
-            <th>Created</th>
+            <th>
+              <Text weight={700} align="center">
+                Title
+              </Text>
+            </th>
+            <th>
+              <Text weight={700} align="center">
+                Link
+              </Text>
+            </th>
+            <th>
+              <Text weight={700} align="center">
+                Note
+              </Text>
+            </th>
+            <th>
+              <Text weight={700} align="center">
+                Category
+              </Text>
+            </th>
+            <th>
+              <Text weight={700} align="center">
+                Created
+              </Text>
+            </th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
