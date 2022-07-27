3b96d3e5fc21b7b7b0c86d02f882bab8bada1fc6
laubernal <lbernalrodrguez@gmail.com>
2022-07-27 17:14:23 +0200

    tested get one method with link filter

--- resources-links/src/modules/resources/Infrastructure/adapters/PrismaResourceFilterAdapter.ts
+++ resources-links/src/modules/resources/Infrastructure/adapters/PrismaResourceFilterAdapter.ts
@@ -1,13 +1,16 @@
-import { Id } from '../../../shared/Domain/vo';
+import { Id, Text } from '../../../shared/Domain/vo';
 import { ResourceFilter } from '../../Domain/filters/ResourceFilter';
 import { Link } from '../../Domain/vo';
+
+type queryType = { where: {}; include: {}; orderBy?: {} };
 
 export class PrismaResourceFilterAdapter {
   constructor(private filter: ResourceFilter) {}
 
   public apply(): any {
     const filters = this.filter.apply();
-    const query: { where: {}; include?: {}; orderBy?: {} } = {
+
+    const query: queryType = {
       where: {},
       include: { categories: true },
       orderBy: { created_at: 'desc' },
@@ -15,31 +18,37 @@ export class PrismaResourceFilterAdapter {
 
     if (filters.has(ResourceFilter.USER_ID_FILTER)) {
       const userId = filters.get(ResourceFilter.USER_ID_FILTER) as Id;
+      const whereQuery = { ...query.where, user_id: { equals: userId.value } };
 
-      Object.assign(query, { where: { user_id: { equals: userId.value } } });
+      Object.assign(query, { where: whereQuery });
     }
 
     if (filters.has(ResourceFilter.RESOURCE_ID_FILTER)) {
       const resourceId = filters.get(ResourceFilter.RESOURCE_ID_FILTER) as Id;
+      const whereQuery = { ...query.where, id: resourceId.value };
 
       delete query.orderBy;
 
-      Object.assign(query, { where: { id: resourceId.value } });
+      Object.assign(query, { where: whereQuery });
     }
 
     if (filters.has(ResourceFilter.TITLE_FILTER)) {
       const title = filters.get(ResourceFilter.TITLE_FILTER) as Text;
-      const whereQuery = { ...query.where, title: { contains: title, mode: 'insensitive' } };
+      const whereQuery = {
+        ...query.where,
+        title: { contains: title, mode: 'insensitive' },
+      };
 
       Object.assign(query, { where: whereQuery });
     }
 
     if (filters.has(ResourceFilter.LINK_FILTER)) {
       const link = filters.get(ResourceFilter.LINK_FILTER) as Link;
+      const whereQuery = { ...query.where, link: link.value };
 
       delete query.orderBy;
 
-      Object.assign(query, { where: { link: link.value } });
+      Object.assign(query, { where: whereQuery });
     }
 
     return query;
